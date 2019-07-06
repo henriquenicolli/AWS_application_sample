e96b883391935cceaf5c807b76cf70d5772003a3
Henrique Nicolli <henriquenicolli@MacBook-Air-de-Henrique.local>
2019-07-06 16:24:47 -0300

    add rekognition

--- src/main/java/com/henrique/nicolli/test/App.java
+++ src/main/java/com/henrique/nicolli/test/App.java
@@ -1,11 +1,13 @@
 
 package com.henrique.nicolli.test;
 
+import java.awt.List;
 import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
@@ -14,6 +16,17 @@ import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.AmazonRekognitionException;
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectLabelsResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.Label;
+import com.amazonaws.services.rekognition.model.S3Object;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.event.S3EventNotification;
 
 /**
@@ -23,25 +36,59 @@ import com.amazonaws.services.s3.event.S3EventNotification;
 public class App implements RequestHandler<S3EventNotification, String> {
 	
 	static final Logger log = LoggerFactory.getLogger(App.class);
+	String bucket;
+	String s3Key;
 	
 
 	@Override
 	public String handleRequest(S3EventNotification s3EventNotification, Context context) {
 		log.info("Lambda function is invoked:" + s3EventNotification.toJson());
 		
-		
 		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
 		DynamoDB dynamoDB = new DynamoDB(client);
 
 		Table table = dynamoDB.getTable("file_data");
 		
-		Item item = new Item()
-				.withPrimaryKey("id",new Random().nextInt())
-			    .withString("log", s3EventNotification.toJson().toString());
-		
-		PutItemOutcome outcome = table.putItem(item);
-		
 
+		for(S3EventNotification.S3EventNotificationRecord record : s3EventNotification.getRecords()) 
+		{	
+			s3Key = record.getS3().getObject().getKey();
+			bucket = record.getS3().getBucket().getName();
+			
+//			AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+//            String body = s3Client.getObjectAsString(bucket, s3Key);
+			AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
+
+		      DetectLabelsRequest request = new DetectLabelsRequest()
+		           .withImage(new Image()
+		           .withS3Object(new S3Object()
+		           .withName(s3Key).withBucket(bucket)))
+		           .withMaxLabels(10)
+		           .withMinConfidence(75F);
+		      
+		      java.util.List <Label> labels = null;
+			
+		      try {
+		          DetectLabelsResult result = rekognitionClient.detectLabels(request);
+		          labels = result.getLabels();
+
+		          System.out.println("Detected labels for " + s3Key);
+		          for (Label label: labels) {
+		             System.out.println(label.getName() + ": " + label.getConfidence().toString());
+		          }
+		       } catch(AmazonRekognitionException e) {
+		          e.printStackTrace();
+		       }  
+		      
+		      Item item = new Item()
+						.withPrimaryKey("id",new Random().nextInt())
+					    .withString("log", labels.toString());
+		      
+		      PutItemOutcome outcome = table.putItem(item);
+
+		}
+
+		
 		return null;
 	}
 
